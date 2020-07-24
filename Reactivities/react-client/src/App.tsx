f16dddd03342869efa51065d9199a342f0ec009f
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-07-24 10:14:30 +0100

    Fetched data from api to populate UI. Client skeleton complete

--- Reactivities/react-client/src/App.tsx
+++ Reactivities/react-client/src/App.tsx
@@ -1,16 +1,33 @@
-import React from "react";
-import { Header, Icon } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="">
-      <Header as="h2">
-        <Icon name="users" />
-        <Header.Content>Reactivities</Header.Content>
-      </Header>
-    </div>
-  );
+class App extends Component {
+  state = { values: [] };
+
+  async componentDidMount() {
+    let { data: values } = await axios.get(
+      "https://localhost:44307/api/values"
+    );
+    this.setState({ values });
+  }
+
+  render() {
+    return (
+      <div className="">
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
 }
 
 export default App;
